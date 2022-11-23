8b200237079040b9d39752a9598502737d726740
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-11-23 02:42:53 +0100

    fix: wait for website load

--- tools/utils.py
+++ tools/utils.py
@@ -1,6 +1,11 @@
+import time
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 import re
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 from credentials import username, password
 
@@ -18,9 +23,10 @@ def login(driver):
 def get_score(driver: WebDriver, username):
     driver.get(f"https://studip.uni-goettingen.de/dispatch.php/profile?username={username}")
     try:
-        score = driver.find_element(By.CSS_SELECTOR,
-                                    "#layout-sidebar > section > div:nth-child(3) > div.sidebar-widget-content > div.profile-sidebar-details > div:nth-child(2) > a > div:nth-child(1)").text
+        score = WebDriverWait(driver, 2).until(EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div/div[5]/div[2]/div[1]/section/div[2]/div[2]/div[2]/div[2]/a/div[1]"))).text
         score = int(score.split(" ")[1].replace(".", ""))
         return score
+
     except:
         return None
