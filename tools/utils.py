b818845e8e50987965a91619b79e59e7d186e338
Lars Kaesberg <33939116+lkaesberg@users.noreply.github.com>
2023-08-26 19:56:51 +0200

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -24,7 +24,7 @@ def get_score(driver: WebDriver, username):
     driver.get(f"https://studip.uni-goettingen.de/dispatch.php/profile?username={username}")
     try:
         score = WebDriverWait(driver, 1).until(EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div/div[5]/div[2]/div[1]/section/div[2]/div[2]/div[2]/div[2]/a/div[1]"))).text
+            (By.XPATH, "/html/body/aside/div[2]/div[2]/div[2]/div[2]/a"))).text
         score = int(score.split(" ")[1].replace(".", ""))
         return score
 
